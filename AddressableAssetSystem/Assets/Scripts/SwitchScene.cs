a227eebf82e53651e379e9a1a0e299cbad154cc5
J.M. Wu <nanxingyiliang@126.com>
2019-08-31 09:09:24 +0800

    Addressable Asset System Demo
    Addressable Asset System Demo

---
+++ AddressableAssetSystem/Assets/Scripts/SwitchScene.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.ResourceProviders;
+
+public class SwitchScene : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+    
+    public void Switch()
+    {
+        Addressables.LoadSceneAsync("SampleScene2", UnityEngine.SceneManagement.LoadSceneMode.Single).Completed += OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(AsyncOperationHandle<SceneInstance> obj)
+    {
+        Debug.Log($"SwitchScene SampleScene2");
+    }
+}
