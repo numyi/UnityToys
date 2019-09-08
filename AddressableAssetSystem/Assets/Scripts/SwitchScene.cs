9b77780e8860229beb2120579dd41c6cdd186f83
J.M. Wu <nanxingyiliang@126.com>
2019-09-09 06:24:05 +0800

    AddressableAssetsSystem project update
    AddressableAssetsSystem project update

--- AddressableAssetSystem/Assets/Scripts/SwitchScene.cs
+++ AddressableAssetSystem/Assets/Scripts/SwitchScene.cs
@@ -1,26 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.AddressableAssets;
-using UnityEngine.ResourceManagement;
 using UnityEngine.ResourceManagement.AsyncOperations;
 using UnityEngine.ResourceManagement.ResourceProviders;
+using UnityEngine.SceneManagement;
 
 public class SwitchScene : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
-    
-    void Update()
-    {
-        
-    }
-    
     public void Switch()
     {
-        Addressables.LoadSceneAsync("SampleScene2", UnityEngine.SceneManagement.LoadSceneMode.Single).Completed += OnSceneLoaded;
+        Addressables.LoadSceneAsync("SampleScene2", LoadSceneMode.Single).Completed += OnSceneLoaded;
     }
 
     void OnSceneLoaded(AsyncOperationHandle<SceneInstance> obj)
