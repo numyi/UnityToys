e556b07334b89ff45cb268930f853bca405eb57d
J.M. Wu <nanxingyiliang@126.com>
2019-10-13 22:33:53 +0800

    DoozyUI Sample Update
    DoozyUI Sample Update

---
+++ DoozyUI/Assets/OrientationIndicator.cs
@@ -0,0 +1,25 @@
+﻿using Doozy.Engine.Orientation;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class OrientationIndicator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnOrientationEvent(DetectedOrientation orientation)
+    {
+        this.GetComponent<TextMeshProUGUI>().text = orientation.ToString();
+    }
+}
